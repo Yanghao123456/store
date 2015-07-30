7a86da04e5e71d56ee91f8e91f02eccc8405e622
Yanghao123456 <clyz.hao.@gmail.com>
2015-07-30 09:26:34 +0800

    do some change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-
 function showImage(obj){
-	alert("aaaaa");
-	document.getElementById("content").style.display = "block";
-	document.getElementById("img_showing").src = document.getElementById(obj.id).src;
-	document.getElementById("imgContent").style.display = "block";
+	generateImageContent();
+	lightbox_mask.style.transition-property = "width";
+	lightbox_mask.style.-webkit-transition-property = "width";
+	ightbox_mask.style.transition-duration = ".5s";
+	lightbox_mask.style.-webkit-transition-duration = ".5s";
+
+}	
+
+function generateImageContent(){
+	var lightbox_mask = document.createElement("DIV");
+	lightbox_mask.id = "lightbox_mask";
+	document.body.appendChild(lightbox_mask);
+	var lightbox_content = document.createElement("DIV");
+	lightbox_content.id = "lightbox_content";
+	lightbox_mask.appendChild(lightbox_content);
+	var myimage = document.createElement("img");
+	myimage.src = obj.src;
+	lightbox_content.appendChild(myimage);
+	return lightbox_mask;
 }
